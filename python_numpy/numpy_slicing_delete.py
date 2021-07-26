99810c5f6bb9a3b15fccea570313034b0b3f08c2
KImSangGeon <tkdrjs7@naver.com>
2021-07-26 15:21:56 +0900

    slice_delete

---
+++ python_numpy/numpy_slicing_delete.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+arr2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr2, end='\n\n')
+
+# 2번쨰 배열까지 100으로 수정
+arr2[0, :3] = 100
+print(arr2)
+
+arr1 = np.arange(10)
+print(arr1, end='\n\n')
+
+# 삭제하고 싶은 배열, 위치
+arr1_delete = np.delete(arr1, 0) 
+print(arr1_delete, end='\n\n')
+
+arr2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr2, end='\n\n')
+arr2_delete_row = np.delete(arr2, 2, axis=0)
+print(arr2_delete_row, end='\n\n')
+
+arr2_delete_cols = np.delete(arr2, 1, axis=1)
+print(arr2_delete_cols, end='\n\n')
