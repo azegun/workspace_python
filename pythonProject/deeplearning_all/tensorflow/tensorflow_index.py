563470a50a73ca23cd594e313e1ad7ce7dde784c
KImSangGeon <tkdrjs7@naver.com>
2021-08-13 10:37:02 +0900

    tensor 인덱싱

---
+++ pythonProject/deeplearning_all/tensorflow/tensorflow_index.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+
+rank_1_tensor = tf.constant([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+print(rank_1_tensor.numpy())
+
+print("First:", rank_1_tensor[0].numpy())
+print("Second:", rank_1_tensor[1].numpy())
+print("Last:", rank_1_tensor[-1].numpy(), end='\n\n')
+
+print("Everything:", rank_1_tensor[:].numpy())
+print("Before 4:", rank_1_tensor[:4].numpy())
+print("From 4 to the end:", rank_1_tensor[4:].numpy())
+print("From 2, before 7:", rank_1_tensor[2:7].numpy())
+print("Every other item:", rank_1_tensor[::2].numpy())
+print("Reversed:", rank_1_tensor[::-1].numpy(), end='\n\n')
+
+
+rank_2_tensor = tf.constant([[1, 2],
+                             [3, 4],
+                             [5, 6]], dtype=tf.float16)
+print(rank_2_tensor, end='\n\n')
+
+print("Second row:", rank_2_tensor[1, :].numpy())
+print("Second column:", rank_2_tensor[:, 1].numpy())
+print("Last row:", rank_2_tensor[-1, :].numpy())
+print("First item in last column:", rank_2_tensor[0, -1].numpy())
+print("Skip the first row:")
+print(rank_2_tensor[1:, :].numpy(), "\n\n")
+
+rank_3_tensor = tf.constant([
+  [[0, 1, 2, 3, 4],
+   [5, 6, 7, 8, 9]],
+  [[10, 11, 12, 13, 14],
+   [15, 16, 17, 18, 19]],
+  [[20, 21, 22, 23, 24],
+   [25, 26, 27, 28, 29]],])
+
+print(rank_3_tensor[:, :, 4])
